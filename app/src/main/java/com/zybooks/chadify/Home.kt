480c68aa44c85e437fd2f49576f4bdb884a9c8d6
Conner7 <Cpan518@gmail.com>
2023-08-08 12:56:56 -0700

    Added Subject Activity Features

---
+++ app/src/main/java/com/zybooks/chadify/Home.kt
@@ -0,0 +1,22 @@
+package com.zybooks.chadify
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class Home : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_subject, container, false)
+    }
+
+
+
+}
--- app/src/main/java/com/zybooks/chadify/MainActivity.kt
+++ app/src/main/java/com/zybooks/chadify/MainActivity.kt
@@ -2,10 +2,39 @@ package com.zybooks.chadify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.zybooks.chadify.databinding.ActivityMainBinding
+import com.zybooks.chadify.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                // this changes the fragment based on the button you press in the nav bar
+                R.id.home -> replaceFragment(SubjectDialogFragment())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
